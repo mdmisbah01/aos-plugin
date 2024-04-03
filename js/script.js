845a1c06995944bf7a02cced168a482b755021c5
Md Misbah Uddin <154965157+mdmisbah01@users.noreply.github.com>
2024-04-03 02:34:29 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// aos
+ AOS.init({
+ 	offset: 100,
+ 	delay: 50,
+ 	duration: 700,
+ 	easing: 'linear',
+ });
+
+// jq
+$(document).ready(function(){
+	//--------------------------- alert
+	// alert("hello");
+	// $('button').click(function(){
+	// alert("welcome");
+	// })
+	
+  });
